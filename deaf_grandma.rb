63f13b6d1cbb1e4f0ff690dd9c0280e262e6329a
Elijah <kimelijah90@gmail.com>
2014-09-11 15:46:14 -0400

    deaf_grandma.rb pt2

--- deaf_grandma.rb
+++ deaf_grandma.rb
@@ -1,10 +1,17 @@
 myPhrase = gets.chomp
-while myPhrase != "BYE"
-  if myPhrase == myPhrase.upcase
-    puts "NO! NOT SINCE #{rand(1930..1951)}!"
+counter = 0
+while counter != 3
+  if myPhrase == "BYE"
+    counter = counter+1
+    puts counter
   else
-    puts "SPEAK UP SUNNY!"
+    counter = 0
+     if myPhrase == myPhrase.upcase
+       puts "NO! NOT SINCE #{rand(1930..1951)}!"
+     else
+       puts "SPEAK UP SUNNY!"
+     end
   end
   myPhrase = gets.chomp
 end
-puts "BYE!"
+puts "FINE! BYE!"
